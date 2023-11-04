6c43a405f4f6b177aeead0ab271fef9334d529db
sajithlaldev <sajithlal65@gmail.com>
2023-11-04 13:20:38 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import makeWASocket, { DisconnectReason, useMultiFileAuthState } from '@whiskeysockets/baileys'
+import { Boom } from '@hapi/boom'
+
+async function connectToWhatsApp () {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: true
+    })
+    var isMessagesent = false;
+    sock.ev.on ('creds.update', saveCreds)
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                connectToWhatsApp()
+            }
+        } else if(connection === 'open') {
+            console.log('opened connection')
+        }
+    })
+    sock.ev.on('messages.upsert', async m => {
+        console.log(JSON.stringify(m, undefined, 2))
+
+        console.log('replying to', m.messages[0].key.remoteJid)
+        if(!isMessagesent){
+            await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there! You said:'+ m.messages[0].message.conversation })
+            isMessagesent = true;
+        }else{
+            isMessagesent = false;
+        }
+    })
+}
+// run in main file
+connectToWhatsApp()
